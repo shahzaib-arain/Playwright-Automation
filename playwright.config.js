6c0c599aecf9e2530b3d61a69fffc67ceb77fcdb
shahzaib-repo <shazaib@tyfora.com>
2026-01-06 16:55:23 +0500

    HTML report of test cases

--- playwright.config.js
+++ playwright.config.js
@@ -3,6 +3,13 @@ const { defineConfig } = require('@playwright/test');
 module.exports = defineConfig({
   testDir: './tests',
   timeout: 120000,
+  
+  // Add this reporter section
+  reporter: [
+    ['html'], // Simple HTML report
+    ['list']  // Shows results in console
+  ],
+  
   use: {
     baseURL: 'https://staging.meandem.vercel.app',
     browserName: 'chromium',
